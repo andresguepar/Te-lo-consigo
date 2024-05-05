90c04b0d40580f1f404f9ce26c7eb90fce63b07e
magicjuandi <118768572+magicjuandi@users.noreply.github.com>
2024-05-04 23:21:00 -0500

    ,

--- User.py
+++
@@ -1,9 +0,0 @@
-import Type
-
-class User :
- def __init__(self, id, name = None, phone = None, email = None,type = Type):
-  self.id = id
-  self.name = name
-  self.phone = phone
-  self.type = type
-  #si
